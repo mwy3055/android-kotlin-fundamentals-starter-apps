6b9858e77aee2dcd1da69627a0fb28e6db2c2a86
hsk <mwy3055@gmail.com>
2021-05-28 21:55:47 +0900

    9.2 WorkManager

--- DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
+++ DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
@@ -17,7 +17,14 @@
 package com.example.android.devbyteviewer
 
 import android.app.Application
+import android.os.Build
+import androidx.work.*
+import com.example.android.devbyteviewer.work.RefreshDataWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 /**
  * Override application to setup background work via WorkManager
@@ -32,6 +39,36 @@ class DevByteApplication : Application() {
      */
     override fun onCreate() {
         super.onCreate()
+        delayedInit()
+    }
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
+    private fun delayedInit() = applicationScope.launch {
         Timber.plant(Timber.DebugTree())
+        setupRecurringWork()
+    }
+
+    /**
+     * Setup WorkManager background job to fetch new network data daily.
+     */
+    private fun setupRecurringWork() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    setRequiresDeviceIdle(true)
+                }
+            }.build()
+        val request = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            RefreshDataWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            request
+        )
     }
 }
